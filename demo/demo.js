506deb03602afba379899a7e52f91b8822023f24
ktangel <lygltys@163.com>
2015-08-07 02:14:46 +0800

    数组去重优化
    优化

--- demo/demo.js
+++ demo/demo.js
@@ -22,7 +22,7 @@ var arrs = ["a", "b", "c", "d", "a", "b", {
 	["ddd", {
 		"as": 0,
 		"ab": 2,
-		"ac": "32"
+		"ac": "3unobjunobj2"
 	}, "eee", 0],
 	["ddd", {
 		"as": 0,
@@ -68,20 +68,20 @@ var arr2 = [{
 	}, "eee", 0],
 	["ddd", {
 		"as": 0,
-		"ab": 1,
+		"ab": 2,
 		"ac": "32"
 	}, "eee", 0],
 	["ddd", {
 		"as": 0,
-		"ab": 1,
-		"ac": "33"
+		"ab": 2,
+		"ac": "32"
 	}, "eee", 0],
 	["ddd", "eee", 0, {
 		"as": 0,
 		"ab": 2,
 		"ac": "32"
 	}],
-	["eeee", "eee", 0],
+	["ddd", "eee", 0],
 	["ddd", "eee", 0],
 	["ddd", "eee", 1]
 ];
@@ -348,18 +348,22 @@ var s8 = {
 	};
 var arr3 = [];
 arr3.push(s1, s2, s3, s4, s5, s6, s7, s8, s9, s10);
+
 function fn1(a) {
 	var _a = a || "lsjaflkj"; //后面的都是测试用
 	return a.length;
 }
+
 function fn2(a) {
 	var _a = a || "lsjaflkj"; //后面的都是测试用
 	return a.length;
 }
+
 function fn3(b) {
 	var _b = a || "lsjaflkj"; //后面的都是测试用
 	return a.length;
 }
+
 function fn4(b) {
 	var _e = a || "lsjaflkj"; //后面的都是测试用
 	return a.length;
@@ -374,30 +378,30 @@ var fw2 = function fn1(a) {
 };
 var fw3 = (function fn2(a) {
 	var _a = a || "lsjaflkj";
-	var fn=function(){
+	var fn = function() {
 		this.ss = 222
 	};
-	fn.a=12;
-	fn = new fn();//后面的都是测试用
+	fn.a = 12;
+	fn = new fn(); //后面的都是测试用
 	return fn;
 })();
 var fw13 = (function fn2(a) {
 	var _a = a || "lsjaflkj";
-	var fn=function(){
+	var fn = function() {
 		this.ss = 222
 	};
-	fn.a=12;
+	fn.a = 12;
 	fn.prototype.bb = 323;
-	fn = new fn();//后面的都是测试用
+	fn = new fn(); //后面的都是测试用
 	return fn;
 })();
 var fw14 = (function fn2(a) {
 	var _a = a || "lsjaflkj";
-	var fn=function(){
+	var fn = function() {
 		this.saas = 222
 	};
-	fn.a=12;
-	fn = new fn();//后面的都是测试用
+	fn.a = 12;
+	fn = new fn(); //后面的都是测试用
 	return fn;
 })();
 var fw4 = function fn2(a) {
@@ -410,21 +414,21 @@ var fw5 = function fn3(a) {
 };
 
 
-var arra= [];
-arra.push(fn1,fn2,fn3,fn4,fw1,fw2,fw3,fw4,fw13,fw14);
+var arra = [];
+arra.push(fn1, fn2, fn3, fn4, fw1, fw2, fw3, fw4, fw13, fw14);
 
-function uniqarr(arr,nosort,exp,unobj) {
+function uniqarr(arr, nosort, exp, unobj) {
 	/*
-	*数组去重函数
-	*parm: 参数arr为要处理的数组，默认以最优的速度处理字符串数组，参数 [nosort] 为true时不对数组重新排序,
-	*当想处理包含对象的数组时使用,请传入exp参数：默认不将对象不会去重,会保留所有对象,如果想对象也去重,传入参数[unobj]，
-	*/
+	 *数组去重函数
+	 *parm: 参数arr为要处理的数组，默认以最优的速度处理字符串数组，参数 [nosort] 为true时不对数组重新排序,
+	 *当想处理包含对象的数组时使用,请传入exp参数：默认不将对象不会去重,会保留所有对象,如果想对象也去重,传入参数[unobj]，
+	 */
 	var retarr = [],
-		_arr = nosort ? arr:arr.sort() ,
+		_arr = nosort ? arr.sort() : arr,
 		hash = {},
-		_exp = exp && true ,
-		_unobj = unobj && true ;
-		cacheobj = [],
+		_exp = exp && true,
+		_unobj = unobj && true;
+	cacheobj = [],
 		str = Object.prototype.toString;
 	if (!_exp) {
 		for (var i = 0, max = _arr.length; i < max; i++) {
@@ -439,6 +443,7 @@ function uniqarr(arr,nosort,exp,unobj) {
 	var unobj = function(obja, objb) {
 		var a = obja,
 			b = objb;
+		console.log(a.length)
 		if (!((typeof a === typeof b) && (typeof a.length === typeof b.length) && (a.length === b.length))) {
 			return false;
 		}
@@ -489,5 +494,39 @@ function uniqarr(arr,nosort,exp,unobj) {
 	}
 	return retarr;
 }
-console.log("ok=====", uniqarr(arra, false,true,'saa'));
+
+function uniq(arr, sopt, unobj) {
+	var _arr = arr,
+		result = [],
+		type,
+		_sopt = sopt && true,
+		_unobj = unobj && true,
+		prev = [],
+		hash = {};
+	for (var i = 0, max = _arr.length; i < max; i++) {
+		var val = _arr[i];
+		if (_sopt) {
+			if (prev !== val) result.push(val);
+			prev.push(val);
+		} else if (unobj) {
+			type = typeof val === 'object';
+			if (type) {
+				val = JSON.stringify(val);
+				console.log(val);
+			}
+			if (!hash[val]) {
+				result.push(_arr[i]);
+				hash[val] = true;
+			}
+		} else {
+			if (!hash[val]) {
+				result.push(_arr[i]);
+				hash[val] = true;
+			}
+		}
+	}
+	return result;
+}
+console.log("ok=====", uniq(arrs, false));
+console.log("ok=====", uniqarr(arr3));
 console.log("ok=====", _.uniq(arrs, false, true));
